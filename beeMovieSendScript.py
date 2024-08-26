290ea5969800f031baf880504d6a7c0b96cc0c27
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-08-26 20:51:36 -0300

    Update and rename beeMovieSendScript.js to beeMovieSendScript.py

--- beeMovieSendScript.py
+++ beeMovieSendScript.py
@@ -1,28 +1,27 @@
-async function enviarScript(scriptText){
-	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
-	main = document.querySelector("#main"),
-	textarea = main.querySelector(`div[contenteditable="true"]`)
-	
-	if(!textarea) throw new Error("Não há uma conversa aberta")
-	
-	for(const line of lines){
-		console.log(line)
-	
-		textarea.focus();
-		document.execCommand('insertText', false, line);
-		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
-		setTimeout(() => {
-			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
-		}, 100);
-		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
-	}
-	
-	return lines.length;
-}
+import time
+import pyautogui
+import pyfiglet
 
-enviarScript(`
+figlet_text = pyfiglet.figlet_format("lucmsilva")
+
+print(figlet_text)
+print(f"Starting in 5 seconds...\nPlease put your Telegram or Discord client on focus now.")
+
+time.sleep(5)
+
+def enviar_script(script_text):
+    linhas = [linha.strip() for linha in script_text.splitlines() if linha.strip()]
+    
+    for linha in linhas:
+        pyautogui.write(linha)
+        pyautogui.press('enter')
+	# 250ms timing to avoid breaking ToS (you are will be banned of anywhere anyways)
+        time.sleep(0.25)
+
+    return len(linhas)
+
+# shrek script
+linhas_enviadas = enviar_script("""
 BEE Movie
 
 Written by Jerry Seinfeld & Andy Robin & Barry Marder & Spike Feresten
@@ -1392,4 +1391,6 @@ I'm sorry. I'm sorry, everyone. Can we stop here?
 I'm not making a major life decision during a production number!
 All right. Take ten, everybody. Wrap it up, guys.
 I had virtually no rehearsal for that.
-`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
+""")
+
+print(f"Código finalizado, {linhas_enviadas} mensagens enviadas")
